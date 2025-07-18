bfe5c27b12eb87b52df51f75fa75101d091cdf59
sharnab <sharnab2008@gmail.com>
2025-07-19 03:16:37 +0600

    delete included

--- delete_info/urls.py
+++ delete_info/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 app_name = 'delete_info'
 urlpatterns = [
-    path('', views.delete, name='index'),
+    path('<int:id>/', views.delete, name='delete'),
 ]
--- delete_info/views.py
+++ delete_info/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from generate_cv.models import Info
 
 # Create your views here.
-def delete(request):
-    return render(request, 'delete_info/index.html')
+def delete(request, id):
+    try:
+        info = get_object_or_404(Info, id=id)
+        info.delete()
+        messages.success(request, 'CV deleted successfully!')
+    except Exception as e:
+        messages.error(request, f'Error deleting CV: {str(e)}')
+    
+    return redirect('home:index')
